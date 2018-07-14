d05a291130b10c47b3c0eaafabbaba34030a314e
Sanjib Pramanick <pramanick_s@India.XoriantCorp.com>
2018-07-14 18:32:35 +0530

    Initial Setup Commited

---
+++ src/main/java/com/n26/transaction/service/TransactionServiceApplication.java
@@ -0,0 +1,12 @@
+package com.n26.transaction.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionServiceApplication.class, args);
+	}
+}
